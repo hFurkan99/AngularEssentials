83b3ada9a9092015a0835b279afb0bcc3887390b
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-11-17 01:53:20 +0300

    Task Management Project

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+  @Input({ required: true }) selected!: boolean;
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+  // //signal
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // select = output<string>();
+
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
 }
